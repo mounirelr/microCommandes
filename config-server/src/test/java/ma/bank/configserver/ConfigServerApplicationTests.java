a26d94baca7d325f3c68f97d53d4ac1ddcff33f0
mounir elrharbali <mounirelrharbali@gmail.com>
2025-12-28 12:38:29 +0100

    first commit

---
+++ config-server/src/test/java/ma/bank/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bank.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
